143b162228a17f9a05bd6c6d6bf28c8da65e12ee
Ehren Lillies <ehren@outlook.com>
2023-01-27 12:16:59 +0000

    code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { round } from 'lodash';
-import { PlayerType } from './models/player-type';
-import { RockPaperScissorsType } from './models/rock-paper-scissors-type';
-import { Round } from './models/round';
-import { GameUtility } from './utilities/game.utility';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +7,4 @@ import { GameUtility } from './utilities/game.utility';
 })
 export class AppComponent {
   title = 'rock-paper-scissors';
-
-  public test(): void{
-    const computerSelection = GameUtility.getComputerSelection();
-    console.log(computerSelection);
-
-    const newGame = GameUtility.newGame("Player 1", "Player 2", PlayerType.Person, PlayerType.Person);
-    console.log(newGame);
-
-    GameUtility.addRound(newGame, RockPaperScissorsType.Rock, RockPaperScissorsType.Paper);
-    GameUtility.addRound(newGame, RockPaperScissorsType.Rock, RockPaperScissorsType.Paper);
-    GameUtility.addRound(newGame, RockPaperScissorsType.Scissors, RockPaperScissorsType.Paper);
-
-    console.log(newGame);
-  }
 }
