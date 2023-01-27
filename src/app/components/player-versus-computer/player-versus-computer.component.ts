3c03ff06fcef9f67a003af58610ce0a6302e9a92
Ehren Lillies <ehren@outlook.com>
2023-01-27 14:54:13 +0000

    code cleanup

--- src/app/components/player-versus-computer/player-versus-computer.component.ts
+++ src/app/components/player-versus-computer/player-versus-computer.component.ts
@@ -70,26 +70,4 @@ export class PlayerVersusComputerComponent implements OnInit, OnDestroy {
       this.gameService.addRound(playerSelection, computerSelection);
     }
   }
-  
-  getResults(): string[]{
-    const results = [];
-    if(this.game) {
-
-      for(const round of this.game.rounds) {
-        let text = `${RockPaperScissorsType[round.player1Selection as RockPaperScissorsType]} vs ${RockPaperScissorsType[round.player2Selection as RockPaperScissorsType]} : `;
-        if(round.winner === 0) {
-          text += "Tie";
-        }
-        if(round.winner === 1) {
-          text += `${this.game.player1Name} wins`;
-        }
-        if(round.winner === 2) {
-          text += `${this.game.player2Name} wins`;
-        }
-
-        results.push(text);
-      }
-    }
-    return results;
-  }
 }
